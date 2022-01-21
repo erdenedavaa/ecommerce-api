3f25e352500bbd6155030d5a868778f1392a8f98
erdenedavaa <erdenedavaa@users.noreply.github.com>
2022-01-21 10:02:59 +0800

    Reusable auth

---
+++ routes/userRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const router = express.Router()
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require('../middleware/authentication')
+
+const {
+  getAllUsers,
+  getSingleUser,
+  showCurrentUser,
+  updateUser,
+  updateUserPassword,
+} = require('../controllers/userController')
+
+router
+  .route('/')
+  .get(authenticateUser, authorizePermissions('admin', 'owner'), getAllUsers)
+
+router.route('/showMe').get(authenticateUser, showCurrentUser)
+router.route('/updateUser').patch(authenticateUser, updateUser)
+router.route('/updateUserPassword').patch(authenticateUser, updateUserPassword)
+
+router.route('/:id').get(authenticateUser, getSingleUser)
+
+// router.get('/', getAllUsers)
+
+module.exports = router
